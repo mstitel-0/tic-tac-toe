c22feb850b3a07817fd5f3abbed4194b4a80ad25
Maksym <makcim2004leb@gmail.com>
2024-04-12 20:25:17 +0200

    added moving on board using arrow keys and pressing enter to confirm input

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+# cd onedrive/робочий стіл/tic-tac-toe
 from game import TicTacToe
+import os
+import msvcrt
+
 
 class TicTacToeGame:
     def __init__(self):
@@ -6,6 +10,7 @@ class TicTacToeGame:
         self.player_names = []
         self.grid_size = 0
         self.game = None
+        self.active_cell = (0, 0)
 
     def start_game(self):
         print("Welcome to Tic-Tac-Toe!")
@@ -15,6 +20,42 @@ class TicTacToeGame:
 
         self.game = TicTacToe(self.num_players, self.player_names, self.grid_size)
         self.play_game()
+
+    def display_board(self):
+        os.system('cls' if os.name == 'nt' else 'clear')  # Clear the screen
+
+        board = self.game.board
+        cell_width = 5  # Width of each cell (including borders and spaces)
+        row_separator = "+".join(["-" * cell_width for _ in range(self.grid_size)])
+
+        for i in range(self.grid_size):
+            print("|", end="")  # Start each row with "|"
+            for j in range(self.grid_size):
+                cell_content = f"[{board[i][j]:^3}]" if (i, j) != self.active_cell else f"[*{board[i][j]}*]"
+                print(cell_content, end="")
+                if j != self.grid_size - 1:
+                    print("|", end="")
+            print("|")
+            if i != self.grid_size - 1:
+                print("+" + row_separator)
+        print()
+
+    def handle_arrow_input(self):
+        while True:
+            key = ord(msvcrt.getch())  # Get the ASCII value of the pressed key
+            if key == 224:  # Arrow key is pressed
+                arrow_key = ord(msvcrt.getch())  # Get the second byte of arrow key input
+                if arrow_key == 72:  # Up arrow
+                    self.active_cell = (max(self.active_cell[0] - 1, 0), self.active_cell[1])
+                elif arrow_key == 80:  # Down arrow
+                    self.active_cell = (min(self.active_cell[0] + 1, self.grid_size - 1), self.active_cell[1])
+                elif arrow_key == 75:  # Left arrow
+                    self.active_cell = (self.active_cell[0], max(self.active_cell[1] - 1, 0))
+                elif arrow_key == 77:  # Right arrow
+                    self.active_cell = (self.active_cell[0], min(self.active_cell[1] + 1, self.grid_size - 1))
+                self.display_board()  # Update the board display with the new active cell
+            elif key == 13:  # Enter key
+                return self.active_cell  # Return the selected cell when Enter is pressed
 
     def get_players_info(self):
         while True:
@@ -57,24 +98,21 @@ class TicTacToeGame:
                 print("It's a draw!")
                 break
 
-    def display_board(self):
-        board = self.game.board
-        for row in board:
-            print("|".join(f" {cell} " if cell else "   " for cell in row))
-            print("|".join(["---" for _ in range(self.grid_size)]))
-        print()
-
     def get_player_move(self):
         while True:
-            try:
-                row = int(input("Enter row number (0 to grid size - 1): "))
-                col = int(input("Enter column number (0 to grid size - 1): "))
-                if 0 <= row < self.grid_size and 0 <= col < self.grid_size:
-                    break
-                else:
-                    print("Invalid move. Please try again.")
-            except ValueError:
-                print("Please enter valid row and column numbers.")
+            self.display_board()  # Display the board with the current active cell highlighted
+            print("Use arrow keys to navigate, then press Enter to select.")
+
+            row, col = self.handle_arrow_input()  # Get the selected cell using arrow keys
+
+            result, winner, _ = self.game.make_move(row, col)
+
+            if result == 'win':
+                print(f"Congratulations! {winner} wins!")
+                break
+            elif result == 'draw':
+                print("It's a draw!")
+                break
 
         return row, col
 
